bddf0ba18c4994ba7762f745a043ee508de68b94
Joy <github.joysaha@gmail.com>
2024-02-28 20:26:24 +0600

    phone show in card from api

--- js/index.js
+++ js/index.js
@@ -7,11 +7,28 @@ const loadPhone = async () =>{
 }
 
 const displayPhones = phones =>{
-    phones.forEach(phones => {
-        console.log(phones)
+
+    const phoneContainer = document.getElementById('phone-container')
+    
+    phones.forEach(element => {
+        
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = `card w-80 pt-6 bg-base-100 shadow-xl`;
+        phoneCard.innerHTML = `
+        <figure><img src="${element.image}" alt="Shoes" /></figure>
+        <div class="card-body">
+          <h2 class="card-title">${element.phone_name}</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+          </div>
+        </div>
+        `;
+        phoneContainer.appendChild(phoneCard)
     });
 }
 
 
 
 loadPhone();
+
